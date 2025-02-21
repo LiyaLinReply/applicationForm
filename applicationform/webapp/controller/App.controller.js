350cfafc59a5ba56d1ffaf1814380350b1e185ac
Liya Lin <l.lin@reply.it>
2025-02-21 09:51:47 +0000

    base

---
+++ applicationform/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("applicationform.controller.App", {
+      onInit() {
+      }
+  });
+});
---
+++ applicationform/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"applicationform/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
